3f322d558965200b1ea93443a23c4080ca6c963d
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-12-11 10:16:03 +0100

    Add files via upload

---
+++ API_REST_Autos_Bueno/src/test/java/com/example/api_rest_autos/ApiRestAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_rest_autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
