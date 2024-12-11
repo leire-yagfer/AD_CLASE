d783ff3e493efaf128bdcd592616b5bfbd898eee
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-12-11 10:44:00 +0100

    Add files via upload

---
+++ 7_agricultoresGranja/src/main/java/jsl/com/farming/entities/Persona.java
@@ -0,0 +1,15 @@
+package jsl.com.farming.entities;
+
+import lombok.*;
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
+
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+}
