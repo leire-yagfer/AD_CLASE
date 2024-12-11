fb3d8a6ff2fb4eba1c8f27b8166b1f31b5905900
leire-yagfer <leire.yagfer.1@educa.jcyl.es>
2024-12-11 10:21:47 +0100

    Delete 7_agricultoresGranja directory

--- 7_agricultoresGranja/src/main/java/jsl/com/farming/repository/GranjaRepository.java
+++
@@ -1,7 +0,0 @@
-package jsl.com.farming.repository;
-
-import jsl.com.farming.entities.Granja;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GranjaRepository extends JpaRepository<Granja, Integer> {
-}
